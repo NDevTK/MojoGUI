c012010dd0d3d30c5f905c48fbbf9d20f188110b
NDevTK <ndevtk@protonmail.com>
2026-01-25 06:45:29 +0000

    fix workflow

--- generate_bindings.py
+++ generate_bindings.py
@@ -1244,12 +1244,12 @@ def generate_js_binding(parsed, global_kind_map={}, file_to_module={}):
  mojo.internal.OpaqueStruct = mojo.internal.OpaqueStruct || {{
    $: {{
      structSpec: {{ name: 'OpaqueStruct', packedSize: 8, fields: [], versions: [{{version: 0, packedSize: 8}}] }},
-     encode: function(value, encoder, byteOffset, bitOffset, nullable) {
-       if (value !== null && value !== undefined) {
+     encode: function(value, encoder, byteOffset, bitOffset, nullable) {{
+       if (value !== null && value !== undefined) {{
          console.warn('[MojoJS] Encoding OpaqueStruct! Field may be missing its real spec.', value);
-       }
+       }}
        encoder.encodeOffset(byteOffset, 0);
-     },
+     }},
      encodeNull: function(encoder, byteOffset) {{ }},
      decode: function(decoder, byteOffset, bitOffset, nullable) {{
        try {{
